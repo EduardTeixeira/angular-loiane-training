c4a979d48c97bd24f73637288d5718a61136e873
EduardTeixeira <eduard.teixeira@adsrock.com.br>
2020-09-10 00:38:18 -0300

    adicionando data-binding

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'https://github.com/EduardTeixeira';
+
+  cursoAngular: boolean = true;
+
+  urlImagem: string = 'http://lorempixel.com/400/200/';
+
+  valorAtual: string = '';
+  valorSalvo: string = '';
+
+  isMouseOver: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  botaoClicado() {
+    alert('Botão Clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+}
