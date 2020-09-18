2de1aea781c520001319fb42b755c660d6107058
EduardTeixeira <eduard.teixeira@adsrock.com.br>
2020-09-18 16:37:21 -0300

    instalando bibliotecas

---
+++ angular-cli-libs-externas/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import * as _ from 'lodash';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-cli-libs-externas';
+
+  list = _.map([1,2,3], (n) => `# ${n}`);
+}
